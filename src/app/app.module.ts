9b64a32774bbe6bb5be32e5ac1225972b21933f9
Wei-HsiangChang <changwei@sheridancollege.ca>
2022-10-05 23:08:22 -0400

    create headerfooter module and bootstrap Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopicComponent } from './topic/topic.component';
+import { HeadfootModule } from './modules/headfoot/headfoot.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopicComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeadfootModule
   ],
   providers: [],
   bootstrap: [AppComponent]
